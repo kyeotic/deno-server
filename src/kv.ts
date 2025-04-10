24b971ff6fad24712bfa1e2519d9aec321f382eb
Tim Kye <tim@kye.dev>
2025-04-09 20:12:30 -0700

    add indexing methods for kv

--- src/kv.ts
+++ src/kv.ts
@@ -1,3 +1,5 @@
+import { isEqual, differenceWith } from 'lodash-es'
+
 export function makeSet(
   name: Deno.KvKeyPart
 ): (...parts: Deno.KvKeyPart[]) => Deno.KvKeyPart[] {
@@ -67,6 +69,65 @@ export async function upsert<T>(
   return item
 }
 
+export function makeIndexer<
+  Row,
+  Ref,
+  Prop extends Row[Prop] extends Array<infer I> ? keyof Row : never,
+  Val extends Row[Prop] extends Array<infer I> ? I : never
+>(
+  prop: keyof Row,
+  keyer: (item: Val) => Deno.KvKeyPart[],
+  selector: (item: Row) => Ref,
+  comparator: (a: Val, b: Val) => boolean = isEqual
+): (
+  txn: Deno.AtomicOperation,
+  value: Row,
+  existing: Row | null
+) => Deno.AtomicOperation {
+  return (txn: Deno.AtomicOperation, value: Row, existing: Row | null) =>
+    syncIndex<Row, Prop, Val, Ref>(
+      txn,
+      value,
+      existing,
+      prop,
+      keyer,
+      comparator,
+      selector
+    )
+}
+
+function syncIndex<
+  Row,
+  Prop extends Row[Prop] extends Array<infer I> ? keyof Row : never,
+  Val extends Row[Prop] extends Array<infer I> ? I : never,
+  Ref
+>(
+  txn: Deno.AtomicOperation,
+  value: Row,
+  existing: Row | null,
+  prop: keyof Row,
+  keyer: (item: Val) => Deno.KvKeyPart[],
+  comparator: (a: Val, b: Val) => boolean,
+  selector: (item: Row) => Ref
+): Deno.AtomicOperation {
+  if (!Array.isArray(value[prop])) {
+    throw new Error('index prop must be an array type')
+  }
+
+  const refs = (existing ? existing[prop] : []) as Val[]
+
+  // create new
+  differenceWith(value[prop], refs, comparator).forEach((e: Val) =>
+    txn.set(keyer(e), selector(value))
+  )
+
+  // // delete old
+  differenceWith(value[prop] ?? [], refs, comparator).forEach((e: Val) =>
+    txn.delete(keyer(e))
+  )
+
+  return txn
+}
 /**
  * Delete all records from the Deno.kv store
  *
@@ -90,4 +151,19 @@ export async function deleteEntireDb(
     count++
   }
   if (debug) console.log(`${count} records deleted`)
+}
+
+export class BaseStore<Type, Key extends Deno.KvKeyPart> {
+  constructor(
+    protected readonly kv: Deno.Kv,
+    protected readonly keyer: ReturnType<typeof makeSet>
+  ) {}
+
+  protected async get(id: Key): Promise<Type | null> {
+    return (await this.kv.get<Type>(this.keyer(id))).value
+  }
+
+  protected async getAll(): Promise<Type[]> {
+    return await listAllValues(this.kv, this.keyer())
+  }
 }
